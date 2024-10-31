a145a87ae6550ce308b3056116426f2eb3dc6edb
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-31 17:31:03 +0300

    22

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -11,8 +11,8 @@ interface Props {
 	}
 }
 
-export default function Page({ params }: Props) {
-	const { id } = params
+export default function Page({ params }: { params: { id: string } }) {
+	// let { id } = params
 
 	return (
 		<main className='flex gap-6 pt-6'>
@@ -29,8 +29,32 @@ export default function Page({ params }: Props) {
 								}
 								alt={''}
 								fill
+								className='object-cover rounded-md'
 							/>
-							<Image src={''} alt={''} width={128} height={128} />
+							<Image
+								src={
+									'https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=600'
+								}
+								alt={''}
+								width={128}
+								height={128}
+								className='object-cover w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring ring-white'
+							/>
+						</div>
+						<h1 className='capitalize mt-20 mb-4 text-xl font-medium'>ahmed mohamed</h1>
+						<div className="flex items-center justify-center gap-12 mb-4">
+							<div className="flex flex-col items-center">
+								<span className="font-medium">123</span>
+								<span className="text-sm">Post</span>
+							</div>
+							<div className="flex flex-col items-center">
+								<span className="font-medium">1.2k</span>
+								<span className="text-sm">Followers</span>
+							</div>
+							<div className="flex flex-col items-center">
+								<span className="font-medium">13k</span>
+								<span className="text-sm">Following</span>
+							</div>
 						</div>
 					</div>
 					<Feed />
